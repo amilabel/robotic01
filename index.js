f558a1e85e0cce9e6542bcdabdaa7d7f336bd095
amila belhacini <107318951+amilabel@users.noreply.github.com>
2023-02-15 15:22:26 +0100

    robotic files

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector(".btn1").addEventListener("click",function(){
+  console.log("on button is got clicked");
+});
+document.querySelector(".btn2").addEventListener("click",function(){
+  console.log("off button is got clicked");
+});
